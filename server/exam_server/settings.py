10327a3a814d580aec9f436526acbe01492b0bcb
kunalmchandak <kmcwankhed2021@gmail.com>
2026-03-04 15:38:05 +0530

    minor changes

--- server/exam_server/settings.py
+++ server/exam_server/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'change-me')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = True
 
